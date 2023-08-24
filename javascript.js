15661ae437a3a346f5adaed80c2552f9dd5ee65e
Ida Hallbæck <idha0001@stud.kea.dk>
2023-08-24 10:52:17 +0200

    and so it begins

---
+++ javascript.js
@@ -0,0 +1,7 @@
+const info = document.querySelector("#info");
+info.textContent = "noget helt andet";
+info.innerHTML = "<h1>Noget nyt og spændende</h1>";
+
+const pic = document.querySelector("img");
+pic.src = "https://picsum.photos/";
+Pic.alt = "billeder";
