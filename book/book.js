5425fc25429c421c1bca19abb4ee978d24d45117
Ida Hallbæck <idha0001@stud.kea.dk>
2023-08-28 10:22:01 +0200

    alle øvelser

---
+++ book/book.js
@@ -0,0 +1,10 @@
+const firstBook = {
+    title: "Ringenes Herre",
+    author: "Tokien",
+    genre: ["Fantasy", "Drama"],
+    pages: 751,
+    hasRead: true,
+    hasIllustrations: false
+}
+
+console.log(firstBook.author)
